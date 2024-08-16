d9255e37fc7b4ab97ec76091aabdc61e2b9a6b0b
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-08-16 14:58:16 +0530

    completed

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyparser = require('body-parser');
+const { doesNotMatch } = require('assert');
+const dns = require('dns');
+const { error } = require('console');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+storeCount = 0
+storeData = {
+
+}
 
 app.use(cors());
+app.use(bodyparser.urlencoded({'extended':false}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,6 +28,24 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl',function(req,res){
+  try {
+    var url = new URL(req.body.url)
+    if(url.protocol !== 'http:' && url.protocol!=='https:'){
+      throw 'invalid url'
+    }
+      storeCount += 1
+      storeData[storeCount] = url
+      let sUrl = {'original_url':url, 'short_url':storeCount}
+      res.json(sUrl) 
+  } catch (error) {
+    res.json({ error: 'invalid url' })
+  }
+  
+})
+app.get('/api/shorturl/:id',function(req,res){
+  res.redirect(storeData[req.params.id])
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
